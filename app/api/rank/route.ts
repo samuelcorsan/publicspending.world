6581103a858d2ce871867e58f111e8801b59b74c
MrlolDev <mrlol.yt.oficial@gmail.com>
2025-04-27 16:28:02 +0200

    Enhance API sorting functionality to support additional criteria: GDP nominal, world GDP share, spending, and revenue.

--- app/api/rank/route.ts
+++ app/api/rank/route.ts
@@ -3,7 +3,7 @@ import data from "../data.json";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const sortBy = searchParams.get("sortBy"); // "population"
+  const sortBy = searchParams.get("sortBy"); // "population" or "gdpNominal" or "worldGdpShare" or "spending" or "revenue"
   const sortOrder = searchParams.get("sortOrder"); // "asc" or "desc"
 
   const sortedData = [...data]
@@ -12,6 +12,34 @@ export async function GET(request: Request) {
         return sortOrder === "asc"
           ? a.population - b.population
           : b.population - a.population;
+      } else if (sortBy === "gdpNominal") {
+        return sortOrder === "asc"
+          ? a.gdpNominal - b.gdpNominal
+          : b.gdpNominal - a.gdpNominal;
+      } else if (sortBy === "worldGdpShare") {
+        return sortOrder === "asc"
+          ? a.worldGdpShare - b.worldGdpShare
+          : b.worldGdpShare - a.worldGdpShare;
+      } else if (sortBy === "spending") {
+        return sortOrder === "asc"
+          ? (a.spending.find((item) => item.name === "Total Spending")
+              ?.amount ?? 0) -
+              (b.spending.find((item) => item.name === "Total Spending")
+                ?.amount ?? 0)
+          : (b.spending.find((item) => item.name === "Total Spending")
+              ?.amount ?? 0) -
+              (a.spending.find((item) => item.name === "Total Spending")
+                ?.amount ?? 0);
+      } else if (sortBy === "revenue") {
+        return sortOrder === "asc"
+          ? (a.revenue.find((item) => item.name === "Total Revenue")?.amount ??
+              0) -
+              (b.revenue.find((item) => item.name === "Total Revenue")
+                ?.amount ?? 0)
+          : (b.revenue.find((item) => item.name === "Total Revenue")?.amount ??
+              0) -
+              (a.revenue.find((item) => item.name === "Total Revenue")
+                ?.amount ?? 0);
       }
       return 0;
     })

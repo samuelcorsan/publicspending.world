c04b02d0c6e6c4efd3ad7b7a6759e6cd8deb1d00
Samuel Corsan <disamprivate@gmail.com>
2025-04-29 13:18:53 +0200

    fix route build error

--- app/api/[country]/route.ts
+++ app/api/[country]/route.ts
@@ -3,9 +3,10 @@ import data from "../data.json";
 
 export async function GET(
   request: Request,
-  { params }: { params: { countryCode: string } }
+  context: { params: { countryCode: string } }
 ) {
-  const { countryCode } = await params;
+  const params = await context.params;
+  const countryCode = params.countryCode;
   const countryData = data.find((item) => item.code === countryCode);
   return NextResponse.json(countryData);
 }
